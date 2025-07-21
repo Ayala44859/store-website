6ebf0d28ddfe50e04bbd5f6f069ea5bdf9c8a0e7
Ayala <ayala44859@gmail.com>
2025-07-22 00:48:30 +0300

    Add Cloudinary configuration to product controller

--- client/src/components/ProductForManager.jsx
+++ client/src/components/ProductForManager.jsx
@@ -3,39 +3,39 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 
 import { deleteProductById } from "../api/productService.js";
-import {removeProductFromList} from '../features/productSlice.js'
+import { removeProductFromList } from '../features/productSlice.js'
 
-const ProductForManager = ({ product,onDelete }) => {
-    let currentUser=useSelector(st=>st.user.currentUser)
+const ProductForManager = ({ product, onDelete }) => {
+    let currentUser = useSelector(st => st.user.currentUser)
     let navigate = useNavigate();
     const dispatch = useDispatch();
 
     const handleDelete = async () => {
         try {
-            console.log("Deleting product with id:", product._id); 
-            await deleteProductById(product._id,currentUser?.token);
+            console.log("Deleting product with id:", product._id);
+            await deleteProductById(product._id, currentUser?.token);
             onDelete(product._id)
             alert("המוצר נמחק בהצלחה!");
         } catch (error) {
             console.error(error);
-            console.log( error); 
+            console.log(error);
             alert("שגיאה במחיקת המוצר.");
         }
     };
 
     return (
         <Card sx={{ maxWidth: 345, margin: '10px', minHeight: '400px', display: 'flex', flexDirection: 'column' }}>
-             <CardMedia
+            <CardMedia
                 component="img"
                 sx={{
-                    height: "50%",
-                    objectFit: "contain",
+                    height: "200px%",
                     width: "50%",
-                    display: "flex", 
-                    justifyContent: "center", 
-                    alignItems: "center", 
+                    objectFit: "contain",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
                 }}
-                image={`http://localhost:5500/api/images/${product.img}`}
+                image={product.img}
                 alt={product.productName}
             />
             <CardContent sx={{ flexGrow: 1 }}>
